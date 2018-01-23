0c3e4e151cb55f4ae45ef6a453d38907d5d86fb1
liman <506156147@qq.com>
2018-01-23 15:22:19 +0800

    add files

---
+++ src/components/Layout.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="">
+  <el-container>
+    <el-header >Header</el-header>
+    <el-container>
+      <el-aside  width="200px">Aside</el-aside>
+      <el-container>
+        <el-main>
+
+          <div></div>
+
+        </el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+  </el-container>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'layout',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
